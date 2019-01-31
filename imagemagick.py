23160d53a8212f1aa4ad46114201065d50844f61
kdy <laasl@naver.com>
2019-01-31 14:46:29 +0900

    add imagemagick def genthumb

---
+++ imagemagick.py
@@ -0,0 +1,35 @@
+#!/busr/bin/env python
+#coding:utf8
+import os
+import sys
+import subprocess
+
+def genThumb(path):
+	"""
+	경로를 입력받으면 썸네일을 만든다.
+	"""
+	if not os.path.exists(src):
+		return "파일이 존재하지 않습니다."
+	if not os.path.isfile(src):
+		return "파일 형태가 아닙니다."
+	if not os.path.exists("/usr/bin/convert"):
+		return "ImageMagick이 설치되지 않았습니다."
+
+	d, f = os.path.split(src)
+	notuse, ext = os.path.splitext(f)
+	dst = d+"/"+f.replace(ext,".jpg")
+	size="360x240"
+	cmds = ["convert", scr, "-thumbnail", size,
+			"-background", "black", "-gravity", "center",
+			"-extent", size, dst]
+	p = subprocess.Popen(cmds, 
+						stdout=subprocess.PIPE, 
+						stderr=subprocess.PIPE)
+	return p.communicate()
+
+if __name__ == "__main__":
+	src= "/project/circle/in/aces_exr/A003C025_150830_R0d0/A003C025_150830_R0D0.078727.exr"
+	stdOut, stdErr = genThumb(src)
+	if stdErr:
+		sys.stderr.write(stdErr)
+	sys.stdout.wirte(stdOut)
